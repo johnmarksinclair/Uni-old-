5e3fefadc6adaa8a0c9a4321fc792528e7a4ceae
johnmarksinclair <45259974+johnmarksinclair@users.noreply.github.com>
2019-12-10 15:28:50 +0000

    ...

--- Year 2/Systems/exams/1819/q2.c
+++ Year 2/Systems/exams/1819/q2.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
-#include <stdlib.io>
-#include <sting.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+    struct node * next;
+    struct node * prev;
+    int val;
+};
+
+struct queue {
+    struct node * head;
+    struct node * tail;
+};
 
 struct queue * new_queue() {
-    struct queue * q = malloc(sizeof(queue));
+    struct queue * q = malloc(sizeof(struct queue));
     q->head = NULL;
     q->tail = NULL;
     return q;
 }
 
 struct node * new_node(int _val) {
-    struct node * new = malloc(sizeof(node));
+    struct node * new = malloc(sizeof(struct node));
     new->next = NULL;
-    new->prev = NULL:
+    new->prev = NULL;
     new->val = _val;
 }
 
