7231f47723c5ae0008d929fc0a8f5b6900a05e34
johnmarksinclair <45259974+johnmarksinclair@users.noreply.github.com>
2019-12-10 18:02:13 +0000

    q2 finished

--- Year 2/Systems/exams/1819/q2.c
+++ Year 2/Systems/exams/1819/q2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "q2.h"
+// structs
+struct node {
+    struct node * next;
+    struct node * prev;
+    int val;
+};
+struct deque {
+    struct node * head;
+    struct node * tail;
+};
 // declarations
 struct node * new_node(int value);
 struct deque * new_deque();
@@ -40,23 +49,53 @@ void push_front_deque(struct deque * this, int value) {
 }
 // pop a value from the front of the deque
 int pop_front_deque(struct deque * this) {
-    int value = this->head->val;
-    this->head = this->head->next;
-    this->head->prev = NULL;
-    return value;
+    if (this->head == NULL) {
+        return -1;
+    } else {
+        int value = this->head->val;
+        this->head = this->head->next;
+        this->head->prev = NULL;
+        return value;
+    }
 }
 // push a value to the back of the deque
 void push_back_deque(struct deque * this, int value) {
-
+    struct node * new = new_node(value);
+    if (this->head == NULL) {
+        this->head = new;
+        this->tail = new;
+    } else {
+        new->prev = this->tail;
+        this->tail->next = new;
+        this->tail = new;
+    }
 }
 // pop a value from the back of the deque
 int pop_back_deque(struct deque * this) {
-
+    if (this->head == NULL) {
+        return -1;
+    } else {
+        int value = this->tail->val;
+        this->tail = this->tail->prev;
+        this->head->next = NULL;
+        return value;
+    }
 }
 // free the memory used by the deque
 void free_deque(struct deque * this) {
 
 }
 int main() {
+    struct deque * q = new_deque();
+    push_front_deque(q, 2);
+    push_back_deque(q, 3);
+    push_front_deque(q, 1);
+    struct node * temp = q->head;
+    printf("%d\n", temp->val);
+    temp = q->tail;
+    printf("%d\n", temp->val);
+    pop_front_deque(q);
+    temp = q->head;
+    printf("%d\n", temp->val);
     return 0;
 }
--- Year 2/Systems/exams/1819/q2.h
+++ Year 2/Systems/exams/1819/q2.h
@@ -1,15 +1,4 @@
 #ifndef Q2.H
 #define Q2.H
 
-struct node {
-    struct node * next;
-    struct node * prev;
-    int val;
-};
-
-struct deque {
-    struct node * head;
-    struct node * tail;
-};
-
 #endif
