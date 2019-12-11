844c9e9d895838921efd83a1e5b2710a96dd9c60
John Sinclair <sinclairjohn72@gmail.com>
2019-12-11 11:25:15 +0000

    finished

--- Year 2/Systems/exams/1819/q2.h
+++ Year 2/Systems/exams/1819/q2.h
@@ -1,4 +1,21 @@
-#ifndef Q2.H
-#define Q2.H
-
+#ifndef Q2_H
+#define Q2_H
+// structs
+struct node {
+    struct node * next;
+    struct node * prev;
+    int val;
+};
+struct deque {
+    struct node * head;
+    struct node * tail;
+};
+// declarations
+struct node * new_node(int value);
+struct deque * new_deque();
+void push_front_deque(struct deque * this, int value);
+int pop_front_deque(struct deque * this);
+void push_back_deque(struct deque * this, int value);
+int pop_back_deque(struct deque * this);
+void free_deque(struct deque * this);
 #endif
