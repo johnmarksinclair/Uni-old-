e55362948ba23a66a092b6c0b73d9b8d23c2e18b
johnmarksinclair <45259974+johnmarksinclair@users.noreply.github.com>
2020-01-09 16:37:16 +0000

    ...

--- Year 2 Java/Algorithms/bonusAssignment/ArithTest.java
+++ Year 2 Java/Algorithms/bonusAssignment/ArithTest.java
@@ -1,13 +1,71 @@
 package bonusAssignment;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+//-------------------------------------------------------------------------
+/**
+ *  Test class for Arith
+ *
+ *  @version 3.1 09/01/20 11:32:15
+ *
+ *  @author  John Sinclair - 16325734
+ */
+
+@RunWith(JUnit4.class)
 public class ArithTest {
+	
+	@Test
+	public void testValidatePrefixOrder() {
+		String[] prefixLiterals = {};
+		assertFalse(Arith.validatePrefixOrder(prefixLiterals));
+	}
+	
+	@Test
+	public void testValidatePostfixOrder() {
+		
+	}
+	
+	@Test
+	public void evaluatePrefixOrder() {
+		
+	}
+	
+	@Test
+	public void evaluatePostfixOrder() {
+		
+	}
 
 	@Test
-	public void testEvalSymb() {
-		assertEquals(1, Arith.evalSymb("+"));
-		assertEquals(0, Arith.evalSymb("3"));
+	public void convertPrefixToPostfix() {
+		
+	}
+
+	@Test
+	public void convertPostfixToPrefix() {
+		
+	}
+
+	@Test
+	public void convertPrefixToInfix() {
+		
+	}
+
+	@Test
+	public void convertPostfixToInfix() {
+		
+	}
+
+	@Test
+	public void evalSymb() {
+		
+	}
+
+	@Test
+	public void performOp() {
+		
 	}
 }
